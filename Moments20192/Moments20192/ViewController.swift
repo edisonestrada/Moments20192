00de39a0d25f41735f1c0231675ef90a970f5a19
Usuario invitado <Guest@PCSAL68743-MEDIALAB-315.local>
2019-12-05 19:24:34 -0500

    creacion de proyecto 2019

---
+++ Moments20192/Moments20192/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moments20192
+//
+//  Created by Usuario invitado on 12/5/19.
+//  Copyright © 2019 Eafit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
